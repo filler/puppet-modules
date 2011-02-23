daca9e6455effc17f4c13f4fb51963c0f01c60a7
Nick Silkey <nick@silkey.org>
2011-02-23 16:14:44 -0500

    erb sourcing needed @ init.pp of module

--- munin-node/manifests/init.pp
+++ munin-node/manifests/init.pp
@@ -13,5 +13,15 @@ class munin-node {
       hasstatus => "true",
   } # service
 
+  file {
+    "munin-node"
+      owner => "root",     
+      group => "root",
+      mode => "644",
+      require => Package["munin-node"]
+      path => "/etc/munin/munin-node.conf"
+      content => template("munin-node/munin-node.conf.erb"),
+  } # file
+
 } # class munin-node
 
